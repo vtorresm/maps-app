e88bf27c8d2b8fd2c90d41393d7d29882b92d550
Victor Alberto Torres Mori <vatorresm88@gmail.com>
2022-01-02 15:36:17 -0500

    Almacenar los lugares en el store

--- src/store/places/actions.ts
+++ src/store/places/actions.ts
@@ -2,29 +2,47 @@ import { ActionTree } from 'vuex';
 import { PlacesState } from './state';
 import { StateInterface } from '../index';
 import { searchApi } from '@/apis';
-import { PlacesResponse } from '@/interfaces/places';
+
+import { Feature, PlacesResponse } from '@/interfaces/places';
 
 const actions: ActionTree<PlacesState, StateInterface> = {
   getInitialLocation({ commit }) {
+    // todo: colocar loading
     navigator.geolocation.getCurrentPosition(
       ({ coords }) =>
         commit('setLngLat', { lng: coords.longitude, lat: coords.latitude }),
       (err) => {
-        console.log(err);
-        throw new Error('Error getting location');
+        console.error(err);
+        throw new Error('No geolocation :( ');
       }
     );
   },
 
   // Todo: colocar el valor de retorno
-  async searchPlacesByTerm({ commit, state }, query: string) {
+  async searchPlacesByTerm(
+    { commit, state },
+    query: string
+  ): Promise<Feature[]> {
+    if (query.length === 0) {
+      commit('setPlaces', []);
+      return [];
+    }
+
+    if (!state.userLocation) {
+      throw new Error('No hay ubicación del usuario');
+    }
+
+    commit('setIsLoadingPlaces');
+
     const resp = await searchApi.get<PlacesResponse>(`/${query}.json`, {
       params: {
         proximity: state.userLocation?.join(','),
       },
     });
 
-    console.log(resp.data.features);
+    commit('setPlaces', resp.data.features);
+
+    return resp.data.features;
   },
 };
 
--- src/store/places/state.ts
+++ src/store/places/state.ts
@@ -1,13 +1,18 @@
+import { Feature } from '@/interfaces/places';
 export interface PlacesState {
   isLoading: boolean;
   userLocation?: [number, number];
+  isLoadingPlaces: boolean;
+  places: Feature[];
 }
 
 function state(): PlacesState {
   return {
-      isLoading: true,
-      userLocation: undefined,
-  }
+    isLoading: true,
+    userLocation: undefined,
+    isLoadingPlaces: false,
+    places: [],
+  };
 }
 
 export default state;
